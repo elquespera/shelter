eafad2d450cad6037ab9e49ff14bbddbd9c009f0
Pavel Grinkevich <pavel.grinkevich@gmail.com>
2022-05-27 03:59:37 +0200

    fix: card slider behavior on resizing

--- pages/main/script.js
+++ pages/main/script.js
@@ -1,15 +1,23 @@
 class Slider {
-    pos = 0;
-    cardWidth = 270 + 90;
-    cardsinView = 3;
+    _pos = 0;
+    _cardWidth = 270 + 40;
+    _cardsInView = 3;
+    _slider = document.querySelector('#pets-slider');
+    _cards = document.querySelectorAll('.pet-card');
     constructor() {
-        this.slider = document.getElementById('pets-slider');
-        this.cards = document.getElementsByClassName('pet-card');
-        if (this.cards.length > 0) {            
-            this.cardWidth = this.cards[0].offsetWidth + 
-                            parseInt(getComputedStyle(this.cards[0]).marginRight);
+        this.update();
+    }
+
+    get count() {
+        return this._cards.length - this._cardsInView + 1;
+    }
+
+    update() {
+        if (this._cards[0]) {            
+            this._cardWidth = this._cards[0].offsetWidth + 
+                parseInt(getComputedStyle(this._cards[0]).marginRight);
         }
-        this.count = this.cards.length - this.cardsinView + 1;
+        this.moveTo(this._pos);
     }
 
     moveTo(position = 0) {
@@ -18,18 +26,26 @@ class Slider {
         } else if (position < 0) {
             position = this.count - 1;
         }
-        this.slider.style.left = -position * this.cardWidth + 'px';        
-        this.pos = position;
+        this._slider.style.left = -position * this._cardWidth + 'px';        
+        this._pos = position;
     }
 
     moveOne(isRight = false) {
-        this.moveTo(this.pos + (isRight ? 1 : -1));
+        this.moveTo(this._pos + (isRight ? 1 : -1));
     }
 }
 
 let slider;
 
-function sliderButtonClick(isRight = false) {
+const checkSlider = () => {
     if (!slider) slider = new Slider();
-    slider.moveOne(isRight); 
+    return slider;
+} 
+
+function sliderButtonClick(isRight = false) {
+    checkSlider().moveOne(isRight); 
 }
+
+window.addEventListener('resize', _ => {
+    checkSlider().update();
+})
